81d374f6aba6bdd40872243a0b29c4ccabb15ba8
21latte <127061330+21latte@users.noreply.github.com>
2023-03-06 09:49:16 +0900

    Create SQL_584_Find Customer Referee.sql

---
+++ SQL_584_Find Customer Referee.sql
@@ -0,0 +1,7 @@
+# Write an SQL query to report the names of the customer that are not referred by the customer with id = 2.
+
+# Return the result table in any order.
+
+SELECT name
+FROM Customer
+WHERE referee_id != 2 or referee_id IS null
