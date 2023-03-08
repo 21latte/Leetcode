81517147c7a822093b1d4662cf84b72d19fa0417
21latte <127061330+21latte@users.noreply.github.com>
2023-03-08 11:14:41 +0900

    Update SQL_1667_Fix Names in a Table.sql

--- SQL_1667_Fix Names in a Table.sql
+++ SQL_1667_Fix Names in a Table.sql
@@ -1,3 +1,9 @@
+# Write an SQL query to fix the names so that only the first character is uppercase and the rest are lowercase.
+
+# Return the result table ordered by user_id.
+
+# The query result format is in the following example.
+
 SELECT user_id, CONCAT(UPPER (SUBSTRING(name, 1, 1)), LOWER (SUBSTRING(name, 2))) AS NAME
 FROM Users
 ORDER BY user_id
